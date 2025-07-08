f207dae5e7440e9ae189f2fe7e92d2234297a6ea
Arcilios <rul039@ucsd.edu>
2025-07-08 16:50:49 +0800

    Fixed URDF for SUPleft arm 3 joints

--- ros-http/src/decode_node/src/image_decoder_node.cpp
+++ ros-http/src/decode_node/src/image_decoder_node.cpp
@@ -1,29 +1,52 @@
 #include "rclcpp/rclcpp.hpp"
-#include "sensor_msgs/msg/compressed_image.hpp"
+#include "std_msgs/msg/string.hpp"
 #include <opencv2/opencv.hpp>
 #include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <openssl/bio.h>
+#include <openssl/evp.h>
 
 class ImageDecoderNode : public rclcpp::Node {
 public:
     ImageDecoderNode() : Node("image_decoder_node") {
-        subscription_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
+        subscription_ = this->create_subscription<std_msgs::msg::String>(
             "/image_raw", 10,
             std::bind(&ImageDecoderNode::callback, this, std::placeholders::_1));
-        RCLCPP_INFO(this->get_logger(), "✅ ImageDecoderNode started, listening to /image_raw");
+        RCLCPP_INFO(this->get_logger(), "ImageDecoderNode started.");
     }
 
 private:
-    rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr subscription_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 
     void callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg) {
-        cv::Mat img = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
+        std::vector<uchar> decoded_bytes = base64_decode(msg->data);
+
+        cv::Mat img = cv::imdecode(msg->data, cv::IMREAD_COLOR);
         if (img.empty()) {
-            RCLCPP_ERROR(this->get_logger(), "❌ Failed to decode CompressedImage");
+            RCLCPP_ERROR(this->get_logger(), "❌ Failed to decode image. ");
             return;
         }
 
         cv::imshow("Decoded Image", img);
         cv::waitKey(1);
+    }
+
+    std::vector<uchar> base64_decode(const std::string &encoded) {
+        BIO *bio, *b64;
+        int decodeLen = encoded.length();
+        std::vector<uchar> buffer(decodeLen);
+
+        bio = BIO_new_mem_buf(encoded.data(), -1);
+        b64 = BIO_new(BIO_f_base64());
+        bio = BIO_push(b64, bio);
+        BIO_set_flags(bio, BIO_FLAGS_BASE64_NO_NL); // no newlines
+
+        int length = BIO_read(bio, buffer.data(), decodeLen);
+        buffer.resize(length);
+        BIO_free_all(bio);
+        return buffer;
     }
 };
 
